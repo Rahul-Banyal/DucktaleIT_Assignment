10324647ce4649c5bc667ffaaa26e5880edb9fc7
Rahul Banyal <rbanyal40@gmail.com>
2022-03-24 19:35:48 +0530

    Add project files.

---
+++ DucktaleIT_Assignment/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using DucktaleIT_Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DucktaleIT_Assignment.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentDBEntities1 _context = new StudentDBEntities1();
+        // GET: Student
+        public ActionResult Index(string searchString)
+        {
+            List<Student> studentList = new List<Student>();
+            studentList = _context.Student.ToList();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ViewBag.SearchString = searchString;
+                searchString = searchString.ToLower();
+                studentList = studentList.Where(s => s.FirstName.ToLower().Contains(searchString) || s.LastName.ToLower().Contains(searchString) || s.ClassName.ToLower().Contains(searchString)).ToList();
+            }
+
+            return View(studentList);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                _context.Student.Add(student);
+                _context.SaveChanges();
+                ViewBag.Message = "Data Insert Successfully";
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message.ToString();
+            }
+            return View();
+        }
+    }
+}
---
+++ DucktaleIT_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DucktaleIT_Assignment.MvcApplication" Language="C#" %>
